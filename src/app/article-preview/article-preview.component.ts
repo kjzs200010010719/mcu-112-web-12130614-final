499e60469444afc54eb3c6536a2cfc00c947aa87
fengu <fengu0719@gmail.com>
2024-01-11 21:22:38 +0800

    喜愛按鈕元件建立與實作

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -1,12 +1,12 @@
-import { NgFor } from '@angular/common';
 import { Component, HostBinding } from '@angular/core';
 
 import { TagListComponent } from '../tag-list/tag-list.component';
+import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
 
 @Component({
   selector: 'app-article-preview',
   standalone: true,
-  imports: [TagListComponent],
+  imports: [FavoriteButtonComponent, TagListComponent],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.css',
 })
